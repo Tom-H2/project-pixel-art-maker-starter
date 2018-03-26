d1b8b47e6cc469a91ef8236eb458c17a97bb382d
Tom-H2 <35784690+Tom-H2@users.noreply.github.com>
2018-03-26 09:41:52 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -8,3 +8,23 @@ function makeGrid() {
 // Your code goes here!
 
 }
+    let grid = $('#pixelCanvas');
+    let rows = $("#inputHeight").val();
+    let columns = $("#inputWidth").val();
+
+    grid.children().remove(); // delete any previous table rows
+
+//Build the grid row by row and then append to the table
+//  project rubrics requires use of for and while loops
+
+    let tableRows = '';
+    let r = 1;
+    while (r <= rows) {
+        tableRows += '<tr>';
+        for (let c=1; c <= columns; c++) {
+            tableRows += '<td></td>';
+        }
+        tableRows += '</tr>';
+        r += 1;
+    } // end while loop
+    $(grid).append(tableRows); // add grid to DOM
